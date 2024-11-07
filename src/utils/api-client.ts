cd3cb83d6091587e8b2fc7d611546b4c12b5abac
gxlpes <guilhermxlopes@gmail.com>
2024-11-07 09:07:10 -0300

    feat: api client for requests

---
+++ src/utils/api-client.ts
@@ -0,0 +1,32 @@
+const baseURL = 'https://api.gnose.app';
+const origin = 'https://gnose.app';
+
+interface ApiResponse<T> {
+  data: T;
+  error: string | null;
+}
+
+const fetchWithBaseUrl = async <T>(path: string, options?: RequestInit): Promise<ApiResponse<T>> => {
+  try {
+    const response = await fetch(`${baseURL}${path}`, {
+      ...options,
+      headers: {
+        'Origin': origin,
+        ...(options?.headers || {}),
+      },
+    });
+    const data = await response.json();
+
+    return {
+      data,
+      error: null,
+    };
+  } catch (error) {
+    return {
+      data: {} as T,
+      error: (error as Error).message,
+    };
+  }
+};
+
+export { fetchWithBaseUrl };
