4ec12fe3e23de325b73349aeef88e11910af69a6
gxlpes <guilhermxlopes@gmail.com>
2024-11-26 16:40:21 +0100

    fix: adding export layout edge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,8 @@ export const metadata: Metadata = {
   description: 'Share your quotes to the world',
 };
 
+export const runtime = 'edge';
+
 export default function RootLayout({
   children,
 }: Readonly<{
