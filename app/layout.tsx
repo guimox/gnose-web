20e83f2d04a2289cc9b0b8948d54d9e530b1dfaf
Guilherme Lopes <guilhermxlopes@gmail.com>
2025-01-28 10:27:53 +0100

    feat: new sidebar component for the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,18 @@
+import { AppSidebar } from '@/components/app-sidebar';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { ModalProvider } from '@/context/ModalContext';
 import type { Metadata } from 'next';
@@ -30,11 +45,35 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} mx-auto w-full antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} mx-auto max-w-[1432px] bg-sidebar antialiased`}
       >
         <TooltipProvider>
           <ModalProvider>
-            <main className="mx-auto w-full max-w-3xl px-4">{children}</main>
+            <SidebarProvider>
+              <AppSidebar />
+              <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                  <div className="flex items-center gap-2 px-4">
+                    <Breadcrumb>
+                      <BreadcrumbList>
+                        <BreadcrumbItem className="hidden md:block">
+                          <BreadcrumbLink href="#">
+                            Building Your Application
+                          </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator className="hidden md:block" />
+                        <BreadcrumbItem>
+                          <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                        </BreadcrumbItem>
+                      </BreadcrumbList>
+                    </Breadcrumb>
+                  </div>
+                </header>
+                <main className="flex flex-1 flex-col gap-4 bg-white p-4 pt-0">
+                  {children}
+                </main>
+              </SidebarInset>
+            </SidebarProvider>
           </ModalProvider>
         </TooltipProvider>
       </body>
